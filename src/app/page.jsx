b9590d58975ef58fe5dc26280c2ce77ccd3bad2a
Antoniel <antonielzeferino0@gmail.com>
2025-01-31 21:39:10 -0300

    add style

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 import HeroIMG from '/public/images/hero-bg.jpeg';
 import Link from 'next/link';
+import ProductsList from '@/components/ProjectsList';
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <section className="relative w-full h-screen flex items-center justify-center text-white px-4">
+      <section className="relative w-full h-[80vh] md:h-screen flex items-center justify-center text-white px-4">
         <Image
           src={HeroIMG}
           layout="fill"
@@ -13,7 +14,6 @@ export default function Home() {
           alt="hero background"
           priority
         />
-        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
         <div className="absolute text-center px-6 max-w-2xl">
           <h1 className="text-2xl md:text-4xl font-bold mb-4">
             Transforme Seu Ambiente com Pinturas de Qualidade
@@ -22,14 +22,16 @@ export default function Home() {
             Destaque cada detalhe com a perfeição que sua casa ou empresa merece.
           </p>
           <div className="flex flex-wrap gap-4 justify-center">
-            <Link href="https://wa.me/5586995407002?text=Olá, gostaria de solicitar um orçamento para pintura" target="_blank">
-              <button className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition md:w-auto">
-                Faça um orçamento
-              </button>
+            <Link
+              href="https://wa.me/5586995407002?text=Olá, gostaria de solicitar um orçamento para pintura"
+              target="_blank"
+              className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition w-full sm:w-auto text-center"
+            >
+              Faça um orçamento
             </Link>
             <Link
               href="/servicos"
-              className="px-6 py-3 border border-white rounded-lg hover:bg-white hover:text-gray-800 transition md:w-auto text-center"
+              className="px-6 py-3 border border-white rounded-lg hover:bg-white hover:text-gray-800 transition w-full sm:w-auto text-center text-white"
             >
               Veja nossos trabalhos
             </Link>
@@ -58,20 +60,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="py-16 text-center w-full">
-        <h2 className="text-2xl md:text-3xl font-semibold mb-6">Nossos Trabalhos</h2>
-        <div className="grid grid-cols-[repeat(auto-fill,_minmax(320px,_1fr))] gap-8">
-        <div className="shadow-lg rounded-lg overflow-hidden">
-            <Image src={HeroIMG} alt="trabalho 1" width={500} height={300} className="object-cover w-full" />
-          </div>
-          <div className="shadow-lg rounded-lg overflow-hidden">
-            <Image src={HeroIMG} alt="trabalho 2" width={500} height={300} className="object-cover w-full" />
-          </div>
-          <div className="shadow-lg rounded-lg overflow-hidden">
-            <Image src={HeroIMG} alt="trabalho 3" width={500} height={300} className="object-cover w-full" />
-          </div>
-        </div>
-      </section>
+      <ProductsList demoMode />
 
       <section className="py-16 bg-blue-600 text-center text-white w-full">
         <h2 className="text-2xl md:text-3xl font-semibold mb-6">Pronto para transformar seu ambiente?</h2>
