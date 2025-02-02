100778652ca939ad964a7a3ce9b63d4fb96b1262
Antoniel <antonielzeferino0@gmail.com>
2025-02-01 21:22:37 -0300

    add service section component

--- src/app/(pages)/servicos/page.jsx
+++ src/app/(pages)/servicos/page.jsx
@@ -1,12 +1,12 @@
+import ServiceSection from "@/components/ServiceSection";
 import image from "../../../../public/images/wall-painted.jpeg";
 import ProductsList from "@/components/ProjectsList";
+import Link from "next/link";
 
 export default function Servicos() {
   return (
     <main
-      className="min-h-screen bg-cover bg-center pt-20 sm:pt-28"
-      style={{ backgroundImage: `url(${image.src})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}
-    >
+      className="min-h-screen bg-cover bg-center pt-20 sm:pt-28 bg-gray-200">
       <section className="text-center mb-16">
         <h1 className="text-3xl font-bold text-gray-900 mb-4">Nossos Serviços</h1>
         <p className="text-lg text-gray-600 max-w-2xl mx-auto mb-8">
@@ -14,54 +14,22 @@ export default function Servicos() {
         </p>
       </section>
 
-      <section className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] py-8 gap-8">
-        <div className="bg-gray-800 text-gray-200 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
-
-          <h2 className="text-2xl font-semibold mb-4">Pintura Residencial</h2>
-          <p className="text-gray-300">
-            Transforme sua casa com pinturas internas e externas de alta qualidade, deixando o ambiente mais agradável e acolhedor.
-          </p>
-        </div>
-
-        <div className="bg-gray-800 text-gray-200 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
-
-          <h2 className="text-2xl font-semibold mb-4">Pintura Comercial</h2>
-          <p className="text-gray-300">
-            Dê uma nova cara para o seu estabelecimento comercial com serviços de pintura especializados para lojas e escritórios.
-          </p>
-        </div>
-
-        <div className="bg-gray-800 text-gray-200 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
-
-          <h2 className="text-2xl font-semibold mb-4">Revestimentos Decorativos</h2>
-          <p className="text-gray-300">
-            Adicione um toque especial aos seus ambientes com revestimentos decorativos e acabamentos exclusivos.
-          </p>
-        </div>
-
-        <div className="bg-gray-800 text-gray-200 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
-
-          <h2 className="text-2xl font-semibold mb-4">Repintura e Retoques</h2>
-          <p className="text-gray-300">
-            Faça sua pintura durar mais com repinturas e retoques que mantêm seu ambiente com aparência sempre nova.
-          </p>
-        </div>
-      </section>
+      <ServiceSection />
 
       <ProductsList />
 
       <section className="bg-blue-600 text-center text-white py-16 px-6">
-        <h2 className="text-3xl font-semibold mb-4">Transforme seu ambiente hoje mesmo</h2>
-        <p className="text-lg mb-6 max-w-2xl mx-auto">
+        <h2 className="text-2xl sm:text-3xl font-semibold mb-4">Transforme seu ambiente hoje mesmo</h2>
+        <p className="text-md sm:text-lg mb-6 max-w-2xl mx-auto">
           Entre em contato para obter um orçamento e transformar sua casa ou estabelecimento com a nossa pintura de qualidade.
         </p>
-        <a
+        <Link
           href="https://wa.me/5511999999999"
           aria-label="Solicite um orçamento para pintura"
           className="px-8 py-4 bg-white text-blue-600 font-semibold rounded-lg shadow-lg hover:bg-gray-100 transition"
         >
           Solicite um Orçamento
-        </a>
+        </Link>
       </section>
     </main>
   );
